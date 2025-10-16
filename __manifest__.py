6ce4f2d477daee5069848f9dd9b595093ca5b991
Sean Thawe <sean.thawe@outlook.com>
2025-10-16 13:00:55 +0200

    fix(manifest): correct corrupted version number

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Product Barcode Generator',
-    '1.1.1',
+    'version': '1.1.1',
     'category': 'Inventory',
     'summary': 'Generate barcodes for products based on internal references',
     'description': """
